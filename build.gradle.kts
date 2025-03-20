ad0d432cc63492874b8ce4c9782a1d68f74d62cf
Roundaround <roundaroundna@gmail.com>
2025-03-19 20:41:16 -0400

    Add blank signs mod to dev environment to test compat

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,7 @@ plugins {
   id("fabric-loom") version "1.10-SNAPSHOT"
   id("roundalib-gradle") version "1.0.0"
 }
+
+dependencies {
+  modImplementation("me.roundaround:blanksigns:1.0.0+1.21.4")
+}
