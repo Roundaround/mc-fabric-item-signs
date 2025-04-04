a843a42620e33e215549140df0ca84ac177a8d13
Roundaround <roundaroundna@gmail.com>
2025-04-04 10:02:52 -0400

    Convert to using Codec in prep for 1.21.5

--- src/main/java/me/roundaround/itemsigns/server/SignItemStorage.java
+++ src/main/java/me/roundaround/itemsigns/server/SignItemStorage.java
@@ -1,40 +1,38 @@
 package me.roundaround.itemsigns.server;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import me.roundaround.itemsigns.attachment.SignItemsAttachment;
 import me.roundaround.itemsigns.generated.Constants;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtElement;
-import net.minecraft.nbt.NbtHelper;
-import net.minecraft.nbt.NbtList;
+import net.minecraft.nbt.NbtOps;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.PersistentState;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class SignItemStorage extends PersistentState {
-  private static final String NBT_SIGNS = "Signs";
-  private static final String NBT_BLOCK_POS = "BlockPos";
-  private static final String NBT_ATTACHMENT = "Attachment";
+  public static final Codec<SignItemStorage> CODEC = RecordCodecBuilder.create((instance) -> instance.group(Codec.list(
+      Entry.CODEC).fieldOf("Signs").forGetter(SignItemStorage::getEntries)).apply(instance, SignItemStorage::new));
 
   private final HashMap<BlockPos, SignItemsAttachment> attachments = new HashMap<>();
 
   private SignItemStorage() {
+    this.markDirty();
+  }
+
+  private SignItemStorage(List<Entry> entries) {
+    for (Entry entry : entries) {
+      this.attachments.put(entry.blockPos(), entry.attachment());
+    }
   }
 
   @Override
   public NbtCompound writeNbt(NbtCompound rootNbt, RegistryWrapper.WrapperLookup registryLookup) {
-    NbtList nbtList = new NbtList();
-    this.attachments.forEach((blockPos, attachment) -> {
-      NbtCompound signNbt = new NbtCompound();
-      signNbt.put(NBT_BLOCK_POS, NbtHelper.fromBlockPos(blockPos));
-      signNbt.put(NBT_ATTACHMENT, attachment.encode(registryLookup));
-      nbtList.add(signNbt);
-    });
-    rootNbt.put(NBT_SIGNS, nbtList);
-
-    return rootNbt;
+    return (NbtCompound) CODEC.encodeStart(registryLookup.getOps(NbtOps.INSTANCE), this).getOrThrow();
   }
 
   public SignItemsAttachment get(BlockPos blockPos) {
@@ -57,30 +55,23 @@ public class SignItemStorage extends PersistentState {
     }
   }
 
-  private static SignItemStorage fromNbt(NbtCompound nbtRoot, RegistryWrapper.WrapperLookup registryLookup) {
-    SignItemStorage manager = new SignItemStorage();
-
-    if (!nbtRoot.contains(NBT_SIGNS)) {
-      return manager;
-    }
-
-    NbtList nbtList = nbtRoot.getList(NBT_SIGNS, NbtElement.COMPOUND_TYPE);
-    for (int i = 0; i < nbtList.size(); i++) {
-      NbtCompound signNbt = nbtList.getCompound(i);
-      BlockPos blockPos = NbtHelper.toBlockPos(signNbt, NBT_BLOCK_POS).orElseThrow();
-      SignItemsAttachment attachment = SignItemsAttachment.decode(signNbt.get(NBT_ATTACHMENT), registryLookup);
-      manager.attachments.put(blockPos, attachment);
-    }
-
-    return manager;
+  private List<Entry> getEntries() {
+    return this.attachments.entrySet().stream().map((entry) -> new Entry(entry.getKey(), entry.getValue())).toList();
   }
 
   public static SignItemStorage getInstance(ServerWorld world) {
     Type<SignItemStorage> persistentStateType = new PersistentState.Type<>(
         SignItemStorage::new,
-        (nbt, registryLookup) -> fromNbt(nbt, world.getRegistryManager()),
+        (nbt, registryLookup) -> CODEC.parse(registryLookup.getOps(NbtOps.INSTANCE), nbt).getOrThrow(),
         null
     );
     return world.getPersistentStateManager().getOrCreate(persistentStateType, Constants.MOD_ID);
+  }
+
+  private record Entry(BlockPos blockPos, SignItemsAttachment attachment) {
+    public static final Codec<Entry> CODEC = RecordCodecBuilder.create((instance) -> instance.group(
+        BlockPos.CODEC.fieldOf("BlockPos").forGetter(Entry::blockPos),
+        SignItemsAttachment.CODEC.fieldOf("Attachment").forGetter(Entry::attachment)
+    ).apply(instance, Entry::new));
   }
 }
