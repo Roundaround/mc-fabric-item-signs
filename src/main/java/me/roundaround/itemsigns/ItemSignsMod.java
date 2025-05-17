17b718d6da2288c79d4d0634822399e7159ef0c4
Roundaround <roundaroundna@gmail.com>
2025-05-17 08:03:58 -0400

    Moved persistent storage cleanup into ServerChunkEvents.CHUNK_LOAD event handler

--- src/main/java/me/roundaround/itemsigns/ItemSignsMod.java
+++ src/main/java/me/roundaround/itemsigns/ItemSignsMod.java
@@ -6,10 +6,16 @@ import me.roundaround.itemsigns.event.LoadFromNbtEvents;
 import me.roundaround.itemsigns.generated.Constants;
 import me.roundaround.itemsigns.server.SignItemStorage;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerChunkEvents;
+import net.minecraft.block.entity.SignBlockEntity;
 import net.minecraft.nbt.NbtOps;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.util.ItemScatterer;
+import net.minecraft.util.math.BlockPos;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.HashMap;
 
 @Entrypoint(Entrypoint.MAIN)
 public class ItemSignsMod implements ModInitializer {
@@ -27,6 +33,29 @@ public class ItemSignsMod implements ModInitializer {
         nbt.put(SignItemsAttachment.NBT_KEY, SignItemsAttachment.CODEC, registries.getOps(NbtOps.INSTANCE), attachment);
       }
       return state;
+    });
+
+    ServerChunkEvents.CHUNK_LOAD.register((world, chunk) -> {
+      SignItemStorage storage = SignItemStorage.getInstance(world);
+      HashMap<BlockPos, SignItemsAttachment> signs = storage.allInChunk(chunk.getPos());
+      chunk.getBlockEntities().forEach((pos, blockEntity) -> {
+        if (blockEntity instanceof SignBlockEntity) {
+          signs.remove(pos);
+        }
+      });
+
+      signs.forEach((pos, attachment) -> {
+        if (attachment != null && !attachment.isEmpty()) {
+          ItemSignsMod.LOGGER.warn(
+              "Attached item data found for sign at {}, but no appropriate sign found. Dropping the attached items.",
+              pos.toShortString()
+          );
+
+          ItemScatterer.spawn(world, pos, attachment.getAll());
+        }
+
+        storage.remove(pos);
+      });
     });
   }
 }
--- src/main/java/me/roundaround/itemsigns/mixin/SerializedChunkMixin.java
+++ src/main/java/me/roundaround/itemsigns/mixin/SerializedChunkMixin.java
@@ -3,38 +3,19 @@ package me.roundaround.itemsigns.mixin;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import me.roundaround.itemsigns.ItemSignsMod;
-import me.roundaround.itemsigns.attachment.SignItemsAttachment;
 import me.roundaround.itemsigns.event.LoadFromNbtEvents;
-import me.roundaround.itemsigns.server.SignItemStorage;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.RegistryWrapper;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
-import net.minecraft.world.chunk.ProtoChunk;
 import net.minecraft.world.chunk.SerializedChunk;
-import net.minecraft.world.poi.PointOfInterestStorage;
-import net.minecraft.world.storage.StorageKey;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.HashMap;
-import java.util.List;
 
 @Mixin(SerializedChunk.class)
 public abstract class SerializedChunkMixin {
-  @Shadow
-  @Final
-  private List<NbtCompound> blockEntities;
-
   @WrapOperation(
       method = "method_61797", at = @At(
       value = "INVOKE",
@@ -54,43 +35,5 @@ public abstract class SerializedChunkMixin {
     BlockState adjustedState = LoadFromNbtEvents.BLOCK_ENTITY.invoker()
         .beforeBlockEntityLoaded(nbt, world, pos, state, registries);
     return original.call(pos, adjustedState, nbt, registries);
-  }
-
-  @Inject(
-      method = "convert", at = @At(
-      value = "INVOKE",
-      target = "Lnet/minecraft/world/chunk/WorldChunk;<init>(Lnet/minecraft/world/World;" +
-               "Lnet/minecraft/util/math/ChunkPos;Lnet/minecraft/world/chunk/UpgradeData;" +
-               "Lnet/minecraft/world/tick/ChunkTickScheduler;Lnet/minecraft/world/tick/ChunkTickScheduler;" +
-               "J[Lnet/minecraft/world/chunk/ChunkSection;Lnet/minecraft/world/chunk/WorldChunk$EntityLoader;" +
-               "Lnet/minecraft/world/gen/chunk/BlendingData;)V"
-  )
-  )
-  private void beforeCreatingWorldChunk(
-      ServerWorld world,
-      PointOfInterestStorage poiStorage,
-      StorageKey key,
-      ChunkPos expectedPos,
-      CallbackInfoReturnable<ProtoChunk> cir
-  ) {
-    ServerWorld serverWorld = world.toServerWorld();
-    SignItemStorage storage = SignItemStorage.getInstance(serverWorld);
-    HashMap<BlockPos, SignItemsAttachment> signs = storage.allInChunk(expectedPos);
-    for (NbtCompound nbt : this.blockEntities) {
-      signs.remove(BlockEntity.posFromNbt(expectedPos, nbt));
-    }
-
-    signs.forEach((pos, attachment) -> {
-      if (attachment != null && !attachment.isEmpty()) {
-        ItemSignsMod.LOGGER.warn(
-            "Attached item data found for sign at {}, but no appropriate sign found. Dropping the attached items.",
-            pos.toShortString()
-        );
-
-        ItemScatterer.spawn(serverWorld, pos, attachment.getAll());
-      }
-
-      storage.remove(pos);
-    });
   }
 }
